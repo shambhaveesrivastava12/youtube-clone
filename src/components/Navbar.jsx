a36b391117f1d0827811b6c588d6dd42bb02b395
shambhaveesrivastava12 <shambhaveesrivastava12@gmail.com>
2024-12-14 20:14:41 +0530

    edited 2.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => (
       color="white"
       sx={{ flexGrow: 1, textAlign: "center" }}
     >
-      Hello, I am Shambhavi
+      Hello, I am Shambhavie
     </Typography>
     <SearchBar />
   </Stack>
