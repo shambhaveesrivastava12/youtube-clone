22606e20c739ff4a3e51e10284d5e36321fe1e83
shambhaveesrivastava12 <shambhaveesrivastava12@gmail.com>
2024-12-14 19:47:04 +0530

    changes made by shambhavee

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,31 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
 import { SearchBar } from "./";
 
 const Navbar = () => (
-  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <Typography
+      variant="h6"
+      color="white"
+      sx={{ flexGrow: 1, textAlign: "center" }}
+    >
+      Hello, I am Shambhavee
+    </Typography>
     <SearchBar />
   </Stack>
 );
