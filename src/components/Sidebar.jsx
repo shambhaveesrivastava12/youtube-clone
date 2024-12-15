b41b9eaeea49b612113f9446b0b53602d4fc9108
shambhaveesrivastava12 <shambhaveesrivastava12@gmail.com>
2024-12-15 13:04:44 +0530

    changed the sidebar color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,10 +22,17 @@ const Categories = ({ selectedCategory, setSelectedCategory }) => (
         }}
         key={category.name}
       >
-        <span style={{ color: category.name === selectedCategory ? "white" : "blue", marginRight: "15px" }}>
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "green",
+            marginRight: "15px",
+          }}
+        >
           {category.icon}
         </span>
-        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
+        <span
+          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
+        >
           {category.name}
         </span>
       </button>
